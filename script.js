e733a200216d4431d43f6634e2ab670c5ff3c867
evgenyfokin <fokin_e@inbox.ru>
2022-06-13 18:28:14 +0700

    The tip counter is done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+let amount = document.getElementById('amount');
+let guests = document.getElementById('guests');
+let quality = document.getElementById('quality');
+let tipAmount = document.getElementById('tip-amount');
+
+function calculate() {
+    const tip = ((amount.value * quality.value) / guests.value).toFixed();
+    console.log(tip);
+    amount.value = '';
+    guests.value = '';
+    quality.value = '';
+    if (tip === 'NaN') {
+        tipAmount.innerHTML = 'Tip $0 each';
+        showTipAmount();
+    } else {
+        tipAmount.innerHTML = `Tip $ ${tip} each`;
+        showTipAmount();
+    }
+}
+
+function showTipAmount() {
+    let x = tipAmount;
+    x.className = 'show';
+    setTimeout(function () {
+        x.className = x.className.replace('show', '');
+    }, 3000);
+}
